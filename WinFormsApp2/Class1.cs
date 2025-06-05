f3277e695682c036ad2cc5c7ac5dd37df10159cb
alexandra <alexandradumitrescu04@gmail.com>
2025-06-05 11:22:58 +0300

    Add project files.

---
+++ WinFormsApp2/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp2
+{
+    internal class CosCumparaturi
+    {
+        public int NumarProduse;
+        //indexer
+
+        //valoare totala
+
+        //evenimente
+
+        //adaugare produs
+
+        //stergere produs
+
+        //modificare produs- la modificarea datelor unui
+        //obiect din colectie sau la inlocuirea unui obiect
+
+
+
+    }
+}
---
+++ WinFormsApp2/Class2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp2
+{
+    public class Produs:  INotifyPropertyChanged,IComparable<Produs>//,*///IComparable<Produs>
+    {
+        private int cod;
+        private string denumire;
+        private decimal pret;
+        private int cantitate;
+        public Produs(int cod, string denumire, decimal pret, int cantitate)
+        {
+            Cod = cod;
+            Denumire = denumire;
+            Pret= pret;
+            Cantitate = cantitate;
+        }
+        
+
+        public int Cod {  get { return cod; } set { cod = value;
+                OnPropertyChanged(nameof(Cod));
+            } }
+        public decimal Pret { get { return pret; } set { pret = value;
+                OnPropertyChanged(nameof(Pret));
+
+            } }
+        public int Cantitate { get { return cantitate; } set { cantitate=value;
+                OnPropertyChanged(nameof(Cantitate));
+                    } }
+        public string Denumire { get { return denumire; } set { denumire = value;
+                OnPropertyChanged(nameof(Denumire));
+                    } }
+        //public interface INotifyPropertyChanged
+        //{
+        //    event PropertyChangedEventHandler PropertyChanged;
+        //}
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public int CompareTo(Produs other)
+        {
+            return Denumire.CompareTo(other.Denumire);
+        }
+
+
+    }
+}
